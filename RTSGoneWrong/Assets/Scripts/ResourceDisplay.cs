52f9a5f4a0f4149077c218fc74cb8052c5d0b25a
Pratik5295 <36683380+Pratik5295@users.noreply.github.com>
2018-04-02 10:56:10 -0400

    Resource Generator

---
+++ RTSGoneWrong/Assets/Scripts/ResourceDisplay.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ResourceDisplay : MonoBehaviour {
+
+    // Use this for initialization
+    
+    public Text resourcetext;
+    private bool gameover = false;
+    private int favour = 100;
+    
+    
+	void Start () {
+        StartCoroutine(slowtime());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        Display();
+	}
+
+    void Display()
+    {
+        
+        Debug.Log("\nResource: " + favour);
+        resourcetext.text = "Resource: " + favour;
+
+    }
+    void Increase_favour()
+    {
+
+        favour = favour + 1;
+      
+       
+    }
+    IEnumerator slowtime()
+    {
+        WaitForSeconds wait = new WaitForSeconds(5);
+        while (!gameover)
+        {
+
+            Increase_favour();
+            yield return wait;
+        }
+    }
+
+}
---
+++ RTSGoneWrong/Assets/Scripts/ResourceGen.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ResourceGen : MonoBehaviour {
+
+    // Use this for initialization
+    public int favour = 100;
+    private bool gameover = false;
+	void Start () {
+        StartCoroutine(slowtime());
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        
+    }
+
+    void Increase_favour()
+    {
+      
+        favour = favour + 1;
+        GetFavour();
+       // Debug.Log("\nResource: " + favour);
+    }
+
+    IEnumerator slowtime()
+    {
+        WaitForSeconds wait = new WaitForSeconds(5);
+        while (!gameover)
+        {
+            
+            Increase_favour();
+            yield return wait;
+        }
+    }
+
+    public int GetFavour()
+    {
+        return favour;
+    }
+}
